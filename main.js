b096e555abc80abb0fd73f9864f60104c72d859e
RafaGarbin <107377830+RafaGarbin@users.noreply.github.com>
2023-10-30 21:43:04 -0300

    ok

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const aplausos= document.querySelector("#som_tecla_aplausos")
 function tocaSom(som){
     alert('clap clap clap')
 }
-let controle="tocar";
+let controle="tcar";
 if(controle=="tocar"){
     alert('Áudio pronto para tocar')
 }else{alert('O áudio não esta pronto')}
